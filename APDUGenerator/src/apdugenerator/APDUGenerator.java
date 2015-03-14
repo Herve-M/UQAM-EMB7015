fa3ca8d01ad4cbaf953f69a22f564e44f94fd870
Herve-M <herve.matysiak@viacesi.fr>
2015-03-14 15:59:08 -0400

    Base App.

---
+++ APDUGenerator/src/apdugenerator/APDUGenerator.java
@@ -0,0 +1,223 @@
+package apdugenerator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+
+import java.security.*;
+import java.security.spec.*;
+import java.security.interfaces.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.smartcardio.*;
+
+/**
+ *
+ * @author Hervé
+ */
+public class APDUGenerator {
+
+    //CLA ID
+    static final byte CLA_APPLET = (byte) 0xB0;
+
+    //
+    static final byte[] APPLET_AID = {(byte) 0xC3, (byte) 0x5E,
+        (byte) 0x4F, (byte) 0x14, (byte) 0x37, (byte) 0x6B};
+
+    //Applet State
+    static final byte STATE_INIT = 0;
+    static final byte STATE_ISSUED = 1;
+
+    ////INSTRUCTION
+    //INIT
+    static final byte INS_SET_PUBLIC_MODULUS = (byte) 0x01;
+    static final byte INS_SET_PRIVATE_MODULUS = (byte) 0x02;
+    static final byte INS_SET_PRIVATE_EXP = (byte) 0x03;
+    static final byte INS_SET_PUBLIC_EXP = (byte) 0x04;
+    static final byte INS_SET_OWNER_PIN = (byte) 0x05;
+    static final byte INS_SET_ISSUED = (byte) 0x06;
+    //ISSUED
+    static final byte INS_VERIFICATION = (byte) 0x10;
+    static final byte INS_CREDIT = (byte) 0x20;
+    static final byte INS_DEBIT = (byte) 0x30;
+    static final byte INS_BALANCE = (byte) 0x40;
+
+    /**
+     * Generate a Private/Public key pair
+     */
+    private void generateKeyPair() {
+        try {
+            System.out.println("Generating keys...");
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(512);
+            KeyPair keypair = generator.generateKeyPair();
+            RSAPublicKey publicKey = (RSAPublicKey) keypair.getPublic();
+            RSAPrivateKey privateKey = (RSAPrivateKey) keypair.getPrivate();
+
+            FileOutputStream publicKeyFile = new FileOutputStream("public.key");
+            publicKeyFile.write(publicKey.getEncoded());
+            publicKeyFile.close();
+
+            FileOutputStream privateKeyFile = new FileOutputStream("private.key");
+            privateKeyFile.write(privateKey.getEncoded());
+            privateKeyFile.close();
+
+            System.out.println("Modulus = " + publicKey.getModulus());
+            System.out.println("Public Exp = " + publicKey.getPublicExponent());
+            System.out.println("Private Exp = " + privateKey.getPrivateExponent());
+
+        } catch (Exception ex) {
+            Logger.getLogger(APDUGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Generic loading func.
+     * @param fileName
+     * @return 
+     */
+    private byte[] loadFile(String fileName) {
+        File file = new File(fileName);
+        FileInputStream fileInputStream = null;
+
+        try {
+            fileInputStream = new FileInputStream(file);
+
+            System.out.println("Total file size to read (in bytes) : "
+                    + fileInputStream.available());
+
+            int length = fileInputStream.available();
+            byte[] data = new byte[length];
+            fileInputStream.read(data);
+            fileInputStream.close();
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Convert BigInt to Byte[]
+     * @param big
+     * @return 
+     */
+    byte[] getBytes(BigInteger big) {
+        byte[] data = big.toByteArray();
+        if (data[0] == 0) {
+            byte[] tmp = data;
+            data = new byte[tmp.length - 1];
+            System.arraycopy(tmp, 1, data, 0, tmp.length - 1);
+        }
+        return data;
+    }
+
+    /**
+     * Make a displayable byte[]
+     * @param in
+     * @return 
+     */
+    String byteToStr(byte[] in) {
+        StringBuilder out = new StringBuilder();
+        for (byte b : in) {
+            out.append("0x"+String.format("%02X ", b));
+        }
+        return out.toString();
+    }
+
+    public void setPublicKey() {
+        try {
+            byte[] data = loadFile("public.key");
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            RSAPublicKey key = (RSAPublicKey) factory.generatePublic(spec);
+
+            byte[] modulus = getBytes(key.getModulus());
+
+            CommandAPDU capdu;
+            capdu = new CommandAPDU(CLA_APPLET, INS_SET_PUBLIC_MODULUS, (byte) 0,
+                    (byte) 0, modulus);
+            System.out.println("APDU for setting Public Key Modulus :");
+            System.out.println(byteToStr(capdu.getBytes()));
+
+            byte[] exponent = getBytes(key.getPublicExponent());
+            capdu = new CommandAPDU(CLA_APPLET, INS_SET_PUBLIC_EXP, (byte) 0,
+                    (byte) 0, exponent);
+            System.out.println("APDU for setting Public Key Exp :");
+            System.out.println(byteToStr(capdu.getBytes()));
+        } catch (Exception ex) {
+            Logger.getLogger(APDUGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void setPrivateKey(){
+        try{
+            byte[] data = loadFile("private.key");
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(data);
+            KeyFactory factory = KeyFactory.getInstance("RSA");
+            RSAPrivateKey key = (RSAPrivateKey) factory.generatePrivate(spec);
+            
+            
+            byte[] modulus = getBytes(key.getModulus());
+            CommandAPDU capdu;
+            capdu = new CommandAPDU(CLA_APPLET, INS_SET_PRIVATE_MODULUS, (byte) 0,
+                    (byte) 0, modulus);
+            System.out.println("APDU for setting Private Key Modulus :");
+            System.out.println(byteToStr(capdu.getBytes()));
+            
+            byte[] exponent = getBytes(key.getPrivateExponent());
+            capdu = new CommandAPDU(CLA_APPLET, INS_SET_PRIVATE_EXP, (byte) 0,
+					(byte) 0, exponent);
+            System.out.println("APDU for setting Private Key Exp :");
+            System.out.println(byteToStr(capdu.getBytes()));
+        } catch (Exception ex) {
+            Logger.getLogger(APDUGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+   
+    public void printMenu(){
+        System.out.println("Select one5 option :");
+        System.out.println("1 - Generate KeyPair");
+        System.out.println("2 - Get Private Key APDU");
+        System.out.println("3 - Get Public Key APDU");
+        System.out.println("4 - Quit");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                this.generateKeyPair();
+                break;
+            case 2:
+                this.setPrivateKey();
+                break;
+            case 3:
+                this.setPublicKey();
+                break;
+            case 4:
+                return;
+            default:
+                this.printMenu();
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        APDUGenerator apdug = new APDUGenerator();
+        apdug.printMenu();
+    }
+
+}
---
+++ CryptedBankCard/src/cbc/CryptedBankCard.java
@@ -0,0 +1,372 @@
+package cbc;
+
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.JCSystem;
+import javacard.framework.OwnerPIN;
+import javacard.framework.Util;
+import javacard.security.CryptoException;
+import javacard.security.KeyBuilder;
+import javacard.security.RSAPrivateKey;
+import javacard.security.RSAPublicKey;
+import javacardx.crypto.Cipher;
+
+/**
+ *
+ * @author Hervé
+ */
+public class CryptedBankCard extends Applet {
+
+    //CLA ID
+    static final byte CLA_APPLET = (byte) 0xB0;
+
+    //APPLET STATE
+    static final byte STATE_INIT = 0;
+    static final byte STATE_ISSUED = 1;
+
+    ////INSTRUCTION
+    //INIT
+    static final byte INS_SET_PUBLIC_MODULUS = (byte) 0x01;
+    static final byte INS_SET_PRIVATE_MODULUS = (byte) 0x02;
+    static final byte INS_SET_PRIVATE_EXP = (byte) 0x03;
+    static final byte INS_SET_PUBLIC_EXP = (byte) 0x04;
+    static final byte INS_SET_OWNER_PIN = (byte) 0x05;
+    static final byte INS_SET_ISSUED = (byte) 0x06;
+    //ISSUED
+    static final byte INS_VERIFICATION = (byte) 0x10;
+    static final byte INS_CREDIT = (byte) 0x20;
+    static final byte INS_DEBIT = (byte) 0x30;
+    static final byte INS_BALANCE = (byte) 0x40;
+
+    ////STATUS WORD
+    final static short SW_VERIFICATION_FAILED = 0x6300;
+    final static short SW_PIN_VERIFICATION_REQUIRED = 0x6301;
+    final static short SW_INVALID_TRANSACTION_AMOUNT = 0x6A83;
+    final static short SW_EXCEED_MAXIMUM_BALANCE = 0x6A84;
+    final static short SW_NEGATIVE_BALANCE = 0x6A85;
+
+    //DATA
+    static final byte PIN_MAX_LIMIT = (byte) 0x03;
+    static final byte PIN_MAX_SIZE = (byte) 0x04;
+    private OwnerPIN ownerPIN;
+    private short balance;
+    private byte state;
+    byte[] tmp;
+
+    //SECURITY
+    RSAPrivateKey privateKey;
+    RSAPublicKey publicKey;
+    Cipher cipher;
+
+    //BEHAVIOR
+    final static short MAX_BALANCE = 0x7FFF;
+    final static byte MAX_TRANSACTION_AMOUNT = 127;
+    final static byte PIN_TRY_LIMIT = (byte) 0x03;
+    final static byte MAX_PIN_SIZE = (byte) 0x04;
+
+    /**
+     * Installs this applet.
+     *
+     * @param bArray the array containing installation parameters
+     * @param bOffset the starting offset in bArray
+     * @param bLength the length in bytes of the parameter data in bArray
+     */
+    public static void install(byte[] bArray, short bOffset, byte bLength) {
+        new CryptedBankCard(bArray, bOffset, bLength);
+    }
+
+    /**
+     * Only this class's install method should create the applet object.
+     */
+    protected CryptedBankCard(byte[] bArray, short bOffset, byte bLength) {
+        ownerPIN = new OwnerPIN(PIN_MAX_LIMIT, PIN_MAX_SIZE);
+        //If install param can be modified
+//        ownerPIN.update(bArray, bOffset, bLength);
+        //Cipher
+        cipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
+        //Crypt
+        privateKey = (RSAPrivateKey) KeyBuilder.buildKey(KeyBuilder.TYPE_RSA_PRIVATE, KeyBuilder.LENGTH_RSA_512, false);
+        publicKey = (RSAPublicKey) KeyBuilder.buildKey(KeyBuilder.TYPE_RSA_PUBLIC, KeyBuilder.LENGTH_RSA_512, false);
+        //Applet State
+        state = STATE_INIT;
+        //TMP
+        tmp = JCSystem.makeTransientByteArray((short) 256, JCSystem.CLEAR_ON_RESET);
+        //register
+        register();
+    }
+
+    /**
+     * Processes an incoming APDU.
+     *
+     * @see APDU
+     * @param apdu the incoming APDU
+     */
+    public void process(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+
+        //APPLET Selection
+        if (selectingApplet()) {
+            return;
+        }
+
+        //Read Bin Only
+        if (buffer[ISO7816.OFFSET_CLA] != CLA_APPLET) {
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+        }
+
+        byte ins = buffer[ISO7816.OFFSET_INS];
+
+        switch (state) {
+            case STATE_INIT:
+                short lc = (short) (buffer[ISO7816.OFFSET_LC] & 0x00FF);
+                switch (ins) {
+                    case INS_SET_PUBLIC_MODULUS:
+                        insSetPublicModulus(apdu, lc);
+                        break;
+                    case INS_SET_PRIVATE_MODULUS:
+                        insSetPrivateModulus(apdu, lc);
+                        break;
+                    case INS_SET_PUBLIC_EXP:
+                        insSetPublicExp(apdu, lc);
+                        break;
+                    case INS_SET_PRIVATE_EXP:
+                        insSetPrivateExp(apdu, lc);
+                        break;
+                    case INS_SET_OWNER_PIN:
+                        insSetOwnerPin(apdu);
+                        break;
+                    case INS_VERIFICATION:
+                        insVerification(apdu);
+                        break;
+                    case INS_SET_ISSUED:
+                        insSetIssued();
+                        break;
+                    default:
+                        ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+                }
+                break;
+            case STATE_ISSUED: {
+                if (ins == INS_VERIFICATION) {
+                    insVerification(apdu);
+                } else {
+                    if (!ownerPIN.isValidated()) {
+                        ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
+                    }
+                    switch (ins) {
+                        case INS_BALANCE:
+                            insBalance(apdu);
+                            break;
+                        case INS_CREDIT:
+                            insCredit(apdu);
+                            break;
+                        case INS_DEBIT:
+                            insDebit(apdu);
+                            break;
+                        default:
+                            ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+                    }
+                }
+                break;
+            }
+            default:
+                ISOException.throwIt(ISO7816.SW_COMMAND_NOT_ALLOWED);
+        }
+    }
+
+    /**
+     * Return account balance
+     *
+     * @param apdu
+     */
+    private void insBalance(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+
+        apdu.setOutgoing();
+        apdu.setOutgoingLength((byte) 2);
+
+        Util.setShort(buffer, (short) 0, balance);
+        
+//        cipher.init(publicKey, Cipher.MODE_ENCRYPT);
+//        cipher.doFinal(buffer,ISO7816.OFFSET_LC,ISO7816.OFFSET_LC,buffer,ISO7816.OFFSET_LC);
+
+        apdu.sendBytes((short) 0, (short) 2);
+    }
+
+    /**
+     * Make a credit action
+     *
+     * @param apdu
+     */
+    private void insCredit(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte numBytes = buffer[ISO7816.OFFSET_LC];
+        byte byteRead = (byte) (apdu.setIncomingAndReceive());
+
+        if ((numBytes != 1) || (byteRead != 1)) {
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        }
+
+        byte creditAmount = buffer[ISO7816.OFFSET_CDATA];
+        
+//        cipher.init(privateKey, Cipher.MODE_DECRYPT);
+//        cipher.doFinal(creditAmount,(short)0,(short)0x01,creditAmount,(short)0);
+
+        if ((creditAmount > MAX_TRANSACTION_AMOUNT) || (creditAmount < 0)) {
+            ISOException.throwIt(SW_INVALID_TRANSACTION_AMOUNT);
+        }
+
+        if ((short) (balance + creditAmount) > MAX_BALANCE) {
+            ISOException.throwIt(SW_EXCEED_MAXIMUM_BALANCE);
+        }
+        
+        JCSystem.beginTransaction();
+        balance = (short) (balance + creditAmount);
+        JCSystem.commitTransaction();
+    }
+
+    /**
+     * Make a debit action
+     *
+     * @param apdu
+     */
+    private void insDebit(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte numBytes = buffer[ISO7816.OFFSET_LC];
+        byte byteRead = (byte) (apdu.setIncomingAndReceive());
+
+        if ((numBytes != 1) || (byteRead != 1)) {
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        }
+
+        byte debitAmount = buffer[ISO7816.OFFSET_CDATA];
+        
+//        cipher.init(privateKey, Cipher.MODE_DECRYPT);
+//        cipher.doFinal(debitAmount,(short)0,(short)0x01,debitAmount,(short)0);
+
+        if ((debitAmount > MAX_TRANSACTION_AMOUNT) || (debitAmount < 0)) {
+            ISOException.throwIt(SW_INVALID_TRANSACTION_AMOUNT);
+        }
+
+        if ((short) (balance - debitAmount) < (short) 0) {
+            ISOException.throwIt(SW_NEGATIVE_BALANCE);
+        }
+        JCSystem.beginTransaction();
+        balance = (short) (balance - debitAmount);
+        JCSystem.commitTransaction();
+    }
+
+    /**
+     * Verify PIN
+     *
+     * @param apdu
+     */
+    private void insVerification(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte byteRead = (byte) (apdu.setIncomingAndReceive());
+        
+//        cipher.init(privateKey, Cipher.MODE_DECRYPT);
+//        cipher.doFinal(buffer,ISO7816.OFFSET_CDATA,byteRead,buffer,ISO7816.OFFSET_CDATA);
+
+        if (ownerPIN.check(buffer, ISO7816.OFFSET_CDATA, byteRead) == false) {
+            ISOException.throwIt(SW_VERIFICATION_FAILED);
+        }
+    }
+
+    //TODO Set return data
+    /**
+     * Set Ownper PIN Only used if install paramter can't be modified
+     *
+     * @param apdu
+     */
+    private void insSetOwnerPin(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        JCSystem.beginTransaction();
+        ownerPIN.update(buffer, ISO7816.OFFSET_CDATA, buffer[ISO7816.OFFSET_LC]);
+        JCSystem.commitTransaction();
+    }
+    
+    /**
+     * Set Modulus of public key
+     * @param apdu
+     * @param lc 
+     */
+    void insSetPublicModulus(APDU apdu, short lc){
+        byte[] buffer = apdu.getBuffer();
+        try {
+            JCSystem.beginTransaction();
+            publicKey.setModulus(buffer, ISO7816.OFFSET_CDATA, buffer[ISO7816.OFFSET_LC]);
+            JCSystem.commitTransaction();
+        } catch (CryptoException ex){
+            JCSystem.abortTransaction();
+        }
+    }
+   /**
+    * Set Modulus of private key
+    * @param apdu
+    * @param lc 
+    */
+    void insSetPrivateModulus(APDU apdu, short lc){
+        byte[] buffer = apdu.getBuffer();
+        try{
+            JCSystem.beginTransaction();
+            privateKey.setModulus(buffer, ISO7816.OFFSET_CDATA, buffer[ISO7816.OFFSET_LC]);
+            JCSystem.commitTransaction();
+        }
+        catch(CryptoException ex){
+            JCSystem.abortTransaction();
+        }        
+    }
+    
+    /**
+     * Set Exponent of private key
+     * @param apdu
+     * @param lc 
+     */
+    void insSetPrivateExp(APDU apdu, short lc){
+        byte[] buffer = apdu.getBuffer();
+        try{
+            JCSystem.beginTransaction();
+            privateKey.setExponent(buffer, ISO7816.OFFSET_CDATA, buffer[ISO7816.OFFSET_LC]);
+            JCSystem.commitTransaction();
+        }
+        catch(CryptoException ex){
+            JCSystem.abortTransaction();
+        }        
+    }
+    
+    /**
+     * Set Exponent of public key
+     * @param apdu
+     * @param lc 
+     */
+    void insSetPublicExp(APDU apdu, short lc){
+        byte[] buffer = apdu.getBuffer();
+        try{
+            JCSystem.beginTransaction();
+            privateKey.setExponent(buffer, ISO7816.OFFSET_CDATA, buffer[ISO7816.OFFSET_LC]);
+            JCSystem.commitTransaction();
+        }
+        catch(CryptoException ex){
+            JCSystem.abortTransaction();
+        }        
+    }
+    
+    void insSetIssued(){
+        state = STATE_ISSUED;
+    }
+
+    public boolean select() {
+        //If card is locked 
+        if (ownerPIN.getTriesRemaining() == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public void deselect() {
+        //Reset Tries value
+        ownerPIN.reset();
+    }
+}
